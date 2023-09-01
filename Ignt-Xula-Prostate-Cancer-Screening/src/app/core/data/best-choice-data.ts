9e35b405042f0a7517591cd29d67ae90f92180f1
Jasmine Monquie Lewis <jasmine.lewis@igloonationtech.com>
2023-08-31 21:16:20 -0500

    Module 5: Previous/Next | Best Choice :: Fix Screening Tab

---
+++ Ignt-Xula-Prostate-Cancer-Screening/src/app/core/data/best-choice-data.ts
@@ -0,0 +1,36 @@
+import { ModuleNavTabs } from '../models/module-nav-tabs';
+
+export var BEST_CHOICE_MODULE_NAV_TAB: ModuleNavTabs[] = [
+    { 
+        'href': 'title',   
+        'tab': 'titleTab' 
+    },
+    {
+        'href': 'choice',
+        'tab': 'choiceTab',
+    },
+    {
+        'href': 'bestChoiceQuestionnaire',
+        'tab': 'bestChoiceQuestionnaireTab',
+    },
+    {
+        'href': 'factors',
+        'tab': 'factorsTab',
+    },
+    {
+        'href': 'discussion',
+        'tab': 'discussionTab',
+    },
+    {
+        'href': 'screening',
+        'tab': 'screeningTab',
+    },
+    {   
+        'href': 'summary', 
+        'tab': 'summaryTab' 
+    },
+    {   
+        'href': 'credits', 
+        'tab': 'creditsTab' 
+    }
+];
